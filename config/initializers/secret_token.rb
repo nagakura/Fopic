7652b6c02b8e9843b829a692cfcbf2d066a769ce
nagakura <t10661kn@sfc.keio.ac.jp>
2013-01-04 00:54:31 +0900

    reset 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fopic::Application.config.secret_token = '124fbff90f5401d24f8ae738b00e3584d5f74021232dedbde68d6f961705744ec59ba48b5871a828ba27ff3ec76cb5489bf6b88d548cbf106acb4d66fcc9f94b'
